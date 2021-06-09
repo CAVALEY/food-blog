2186ccffbf38d79890ca47082ca7b579082715bb
Carlos Yujra <elcarlosardilla@gmail.com>
2021-06-09 02:25:59 -0400

    Primer Comit

---
+++ src/assets/getContentCategor.js
@@ -0,0 +1,36 @@
+export default function getContentCategor(){
+
+    const contentCategor = [
+        {
+            texto: "Maxican Grilled Corn Recipe",
+            imagen: "https://i78.servimg.com/u/f78/20/31/61/87/image_20.jpg",
+            classImag: "imagCategor1",
+            classTexto: "texto1"
+        },
+        {
+            texto: "Rost Chiken With Lemon Gravy",
+            imagen: "https://i78.servimg.com/u/f78/20/31/61/87/image_19.jpg",
+            classImag: "imagCategor2",
+            classTexto: "texto2"
+        },
+        {
+            texto: "Avocado Melon salad With Lime Vinagrette",
+            imagen: "https://i78.servimg.com/u/f78/20/31/61/87/image_18.jpg",
+            classImag: "imagCategor3",
+            classTexto: "texto3"
+        },
+        {
+            texto: "Chunky Beef Stew",
+            imagen: "https://i78.servimg.com/u/f78/20/31/61/87/image-10.jpg",
+            classImag: "imagCategor4",
+            classTexto: "texto4"
+        },
+        {
+            texto: "Farmhouse Vegetable And Barley Soup",
+            imagen: "https://i78.servimg.com/u/f78/20/31/61/87/image_21.jpg",
+            classImag: "imagCategor5",
+            classTexto: "texto5"
+        }
+    ]
+    return contentCategor;
+}
